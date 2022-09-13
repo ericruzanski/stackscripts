e0052451dec7a2342453641eb488e4a974452f00
Eric Ruzanski <61092567+ericruzanski@users.noreply.github.com>
2022-09-13 13:12:46 -0500

    Add files via upload

---
+++ basic-metpy-install-ubuntu.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Update package list
+
+apt-get update
+
+# Install and upgrade pip
+
+apt-get install -y python3-pip
+
+python3 -m pip install --upgrade pip
+
+# Install MetPy and its dependencies
+
+pip3 install metpy
