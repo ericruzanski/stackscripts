5414d204c7a97d8d36bbc2547c0656ffe39e9c34
Eric Ruzanski <61092567+ericruzanski@users.noreply.github.com>
2023-10-30 11:38:10 -0400

    Updated basic-metpy-install-ubuntu.sh
    Added a description with reference links. Cleaned things up a bit.

--- basic-metpy-install-ubuntu.sh
+++ basic-metpy-install-ubuntu.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
+################################################################################
+# Script: basic-metpy-install-ubuntu.sh
+# Author: Eric Ruzanski
+# Description: MetPy is a collection of Python packages for reading, visualizing, 
+#              and performing calculations with meteorological (weather) data. 
+#
+# StackScript Repository:
+# https://github.com/ericruzanski/StackScripts/blob/main/basic-metpy-install-ubuntu.sh
+#
+# Metpy Repository: 
+# https://github.com/Unidata/MetPy/tree/main
+#
+# Disclaimer: This script is provided as-is without any warranties.
+################################################################################
 
-# Update package list
+# Update Packages
+apt-get update && apt-get upgrade -y
 
-apt-get update
-
-# Install and upgrade pip
-
-apt-get install -y python3-pip
-
+# Install Python and Pip
+apt-get install python3 python3-pip -y
 python3 -m pip install --upgrade pip
 
 # Install MetPy and its dependencies
-
-pip3 install metpy
+python3 -m pip install metpy
 
 # Confirm the installation by running the following
-#   pip3 show metpy
+#   `pip show metpy`
